8dd17f58616628e0fb8533448f77df4000455a27
7akase <git_config@user.email>
2017-12-14 23:05:35 +0900

    aaa

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,8 @@
 
 module Main where
 
+import Prelude hiding (takeWhile)
+import Data.Char
 import qualified Data.Text as T
 import Data.Attoparsec.Text hiding (take)
 import Control.Applicative 
@@ -11,36 +13,101 @@ import Control.Applicative
 main :: IO ()
 main = do
   print $ parse decimal "1000" `feed` ""
-  print $ parse ymdParser "19800929" `feed` ""
-  let sample_text = "19800929"
-  print $ parse ymdParser sample_text
+  print $ parse node "  abc_2 " `feed` ""
 
-twoOfDecimal :: Parser (Int, Int)
-twoOfDecimal = do
-  left <- decimal
-  char ','
-  right <- decimal
-  return (left, right)
+-- |
+--
 
+newtype NODE = NODE String deriving (Show)
+type INPUT = NODE
+type OUTPUT = NODE
+type INPUTS = [INPUT]
+type OUTPUTS = [OUTPUT]
+data SENS = Posedge NODE | Negedge NODE | Edge NODE deriving (Show)
+type SENSLST = [SENS]
+type EXPR = String
 
--- data LOGIC = FF <|> CL
-
-data YMD = YMD Int Int Int deriving Show
-
-countRead :: Read a => Int -> Parser Char -> Parser a
-countRead i = fmap read . count i
-
-ymdParser :: Parser YMD
-ymdParser = YMD <$> countRead 4 digit <*> countRead 2 digit <*> countRead 2 digit
-
-
-data INST = INST String String deriving Show
+data COMB = COMB EXPR deriving (Show)
+data SEQL = SEQL SENSLST [COMB] deriving (Show)
+data MOD = MOD INPUTS OUTPUTS [COMB] [SEQL] deriving (Show)
+type INSTNAME = String
+type MODNAME = String
+data INST = INST MODNAME INSTNAME [NODE]
 
 
--- instParser :: Parser INST
--- -- parse instParser "block block_u (a, b, c);" --> INST "block" "block_u"
--- instParser = INST <$> takeToken <*> takeToken
+-- |
+--
+-- >>> parse (token digit) "  123  4"
+-- "Done \"4\" \"123\""
+token :: Parser a -> Parser [a]
+token p = skipSpace *> (many p) <* skipSpace
 
-takeToken :: Parser T.Text
-takeToken = Data.Attoparsec.Text.takeWhile (not . isHorizontalSpace)
+isNodeChar :: Char -> Bool
+isNodeChar x = or $ fmap ($ x) [isAlphaNum, (== '_')]
 
+isEndOfExpr :: Char -> Bool
+isEndOfExpr = (== ';')
+
+isSpecialChar :: Char -> Bool
+isSpecialChar c = any (== c) ['(', ')', '\n', '\t', ' ', ',', ';']
+
+node :: Parser NODE
+node = NODE <$> token (satisfy isNodeChar)
+
+sens :: Parser SENS
+sens = do s <- token (satisfy isAlpha) 
+          n <- node
+          return $ case s of
+                     "posedge" -> Posedge n
+                     "negedge" -> Negedge n
+                     otherwise -> Edge n 
+
+expr :: Parser EXPR
+expr = many $ satisfy (not . isEndOfExpr)
+
+comb :: Parser COMB
+comb = do string "assign"
+          skipSpace
+          o <- node
+          string "="
+          skipSpace
+          e <- expr
+          return $ COMB e
+
+seql :: Parser SEQL
+seql = do token $ string "always"
+          token $ char '@'
+          token $ char '('
+          ss <- sepBy sens (char ',')
+          token $ char ')'
+          es <- expr  -- begin end style is not parsed
+          return $ SEQL ss [COMB es]
+          
+
+mod :: Parser MOD
+mod = undefined
+
+modname :: Parser MODNAME
+modname = token (satisfy isAlphaNum)
+
+instname :: Parser INSTNAME
+instname = token (satisfy isAlphaNum)
+
+inst :: Parser INST
+inst = do m <- modname
+          i <- instname
+          token $ char '('
+          ns <- sepBy node (char ',')
+          token $ char ')'
+          token $ char ';'
+          return $ INST m i ns
+
+xxx = "module counter (clk, rst, dout); \
+      \  reg [3:0] cnt; \
+      \  always @(posedge clk) begin \
+      \    if (rst) \
+      \      cnt <= 4'h0; \
+      \    else \
+      \      cnt <= cnt + 1; \
+      \  end\
+      \ "
